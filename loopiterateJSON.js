43bdf692c6256539ea90c02f4e89019341f505dd
Praveenkumar.J <103561511+Praveenkumarj27@users.noreply.github.com>
2022-04-25 08:06:51 +0530

    Add files via upload

---
+++ loopiterateJSON.js
@@ -0,0 +1,33 @@
+var json = [{
+    "name" : "Praveenkumar", 
+    "gender": "male",
+    "age" : "23"
+},
+{
+    "name" : "bharath", 
+    "gender": "female",
+    "age" : "25"
+}];
+//for loop
+for(var i = 0; i < json.length; i++) {
+    var obj = json[i];
+
+    console.log(obj.name);
+    console.log(obj.gender);
+    console.log(obj.age)
+
+}
+//for Each
+json.forEach(function(obj) { console.log(obj.name); });
+
+//for In
+for (var key in json) {
+  console.log(json[key].gender);
+}
+
+//for Of
+let text = "";
+for (let x of json) {
+    console.log(x.age);
+}
+
---
+++ resumeJSON.js
@@ -0,0 +1,44 @@
+let myResume={
+    "basics": {
+      "name": "PRAVEENKUMAR J ",
+      "email": "jpraveenkumar263@gmail.com",
+      "phone": 7708384827,
+      "degree": "B.E",
+      "location": {
+        "address": "12/84,Bharathiyar street,pudhupattu,kallakurichi",
+        "postalCode": 606402,
+        "city": "kallakurichi",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    },
+    "work": [
+      {
+        "company": "Kritilabs Technologies",
+        "position": "Electronic Techincal Support Engineer",
+        "startDate": "21-01-2021",
+        "endDate": "up to date",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Jerusalem College Of Engineering",
+        "department": "Electrical and Electronics",
+        "batch ": 2016-2020,
+        "gpa": 6.7,
+      }
+    ],
+    "skills": [
+      {
+        "name": "java,HTML,CSS,JS",
+        "level": "beginner",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English"
+      }
+    ],
+   
+  }
+  console.log(myResume);
